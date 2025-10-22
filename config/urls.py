7fdfa59968b52be2135554f33437653638529af3
Abduolimov03 <asadbekabduolimov33@gmail.com>
2025-10-22 10:40:21 +0500

    update

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("i18n/", include("django.conf.urls.i18n")),  # set_language shu yerda
+    path("i18n/", include("django.conf.urls.i18n")),
 ]
 
 urlpatterns += i18n_patterns(
@@ -11,4 +13,8 @@ urlpatterns += i18n_patterns(
     path("", include("accounts.urls")),
     path("income/", include("income.urls")),
     path("expenses/", include("expenses.urls")),
+    path("accounts/", include("allauth.urls")),
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
